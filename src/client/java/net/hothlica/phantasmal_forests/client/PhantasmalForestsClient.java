8bb28c36c75722b2fac102e298a73e584cbab97e
Holi <nelotteles@gmail.com>
2026-05-06 23:58:48 -0400

    Initial commit

---
+++ src/client/java/net/hothlica/phantasmal_forests/client/PhantasmalForestsClient.java
@@ -0,0 +1,10 @@
+package net.hothlica.phantasmal_forests.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhantasmalForestsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/hothlica/phantasmal_forests/client/PhantasmalForestsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hothlica.phantasmal_forests.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PhantasmalForestsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
